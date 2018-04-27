5f98661b27e1acdf886a21d30feb8c406877a743
svyatoslav00012 <svyatoslav.shilkov.2012@mail.ru>
2018-04-27 23:54:28 +0300

    initial commit

---
+++ src/main/java/com/example/easynotes/controller/HouseController.java
@@ -0,0 +1,58 @@
+package com.example.easynotes.controller;
+
+import com.example.easynotes.exception.ResourceNotFoundException;
+import com.example.easynotes.model.House;
+import com.example.easynotes.repository.HouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class HouseController {
+
+	@Autowired
+	HouseRepository houseRepository;
+
+	@GetMapping("/houses")
+	public List<House> getAllHouses() {
+		return houseRepository.findAll();
+	}
+
+	@PostMapping("/houses")
+	public House createHouse(@Valid @RequestBody House house) {
+		return houseRepository.save(house);
+	}
+
+	@GetMapping("/houses/{id}")
+	public House getHouseById(@PathVariable(value = "id") Long houseId) {
+		return houseRepository.findById(houseId)
+				.orElseThrow(() -> new ResourceNotFoundException("House", "id", houseId));
+	}
+
+	@PutMapping("/houses/{id}")
+	public House updateHouse(@PathVariable(value = "id") Long houseId,
+							 @Valid @RequestBody House houseDetails) {
+
+		House house = houseRepository.findById(houseId)
+				.orElseThrow(() -> new ResourceNotFoundException("House", "id", houseId));
+
+		house.setAdress(houseDetails.getAdress());
+
+		House updatedHouse = houseRepository.save(house);
+		return updatedHouse;
+	}
+
+	@DeleteMapping("/houses/{id}")
+	public ResponseEntity<?> deleteHouse(@PathVariable(value = "id") Long houseId) {
+		House house = houseRepository.findById(houseId)
+				.orElseThrow(() -> new ResourceNotFoundException("House", "id", houseId));
+
+		houseRepository.delete(house);
+
+		return ResponseEntity.ok().build();
+	}
+}
